18bce528b63b2ebc4dc294a49ad69f4e2a9797a8
TerminalBrilliance <grindside@gmail.com>
2021-05-18 15:23:26 -0500

    Finally got auth0 authentication fully functional

--- routes/api/terms.js
+++ routes/api/terms.js
@@ -2,10 +2,7 @@ const router = require("express").Router();
 const termsController = require("../../controllers/termsController");
 const withAuth = require("../../utils/auth");
 
-router
-	.route("/")
-	.get(termsController.findAll)
-	.post(withAuth, termsController.create);
+router.route("/").get(termsController.findAll).post(termsController.create);
 
 router
 	.route("/:id")
--- server.js
+++ server.js
@@ -1,43 +1,46 @@
 const express = require("express");
-const session = require("express-session");
+// const session = require("express-session");
 
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
-const MongoStore = require("connect-mongodb-session")(session);
+// const MongoStore = require("connect-mongodb-session")(session);
 const routes = require("./routes");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/codehardcache", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-  );
-  
-  const sess = {
-		secret: "PASSWORD",
-		cookie: {},
-		resave: false,
-		saveUninitialized: true,
-		store: new MongoStore({
-			uri: process.env.MONGODB_URI || "mongodb://localhost/codehardcache",
-			collections: "sessions",
-		}),
-	};
+	process.env.MONGODB_URI || "mongodb://localhost/codehardcache",
+	{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+);
+
+// const sess = {
+// 	secret: "PASSWORD",
+// 	cookie: {},
+// 	resave: false,
+// 	saveUninitialized: true,
+// 	store: new MongoStore({
+// 		uri: process.env.MONGODB_URI || "mongodb://localhost/codehardcache",
+// 		collections: "sessions",
+// 	}),
+// };
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+	app.use(express.static("client/build"));
 }
-  
-app.use(session(sess));
+
+// app.use(session(sess));
 app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("*", function (req, res) {
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+app.listen(PORT, function () {
+	console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
